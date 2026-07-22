2bfd00f7157b56e158c209a76c246295d0a14099
effoccoffee <moiseaidan12@gmail.com>
2026-07-22 14:55:04 -0500

    timer v1

--- scenes/main character.gd
+++ scenes/main character.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -470.0
 @onready var sprite_2d = $Sprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
